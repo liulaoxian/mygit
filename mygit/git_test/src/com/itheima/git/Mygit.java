7c0fd935cc8430b40b02e828eeaba585d09421f1
liuzk <liuzk@163.com>
2019-08-12 09:16:13 +0800

    新建项目

---
+++ mygit/git_test/src/com/itheima/git/Mygit.java
@@ -0,0 +1,13 @@
+package com.itheima.git;
+
+public class Mygit {
+    public static void main(String[] args) {
+        if(1+1 == 2){
+            System.out.println("何师傅是色图搜查官");
+            System.out.println("何师傅是色图搜查官");
+            System.out.println("何师傅是色图搜查官");
+            System.out.println("何师傅是色图搜查官");
+            System.out.println("何师傅是色图搜查官");
+        }
+    }
+}
