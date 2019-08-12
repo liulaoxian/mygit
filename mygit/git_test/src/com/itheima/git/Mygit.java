cd2976f2952336fd6f6b87f1c1826a4ab4bd282d
liuzk <liuzk@163.com>
2019-08-12 09:48:02 +0800

    修改

--- mygit/git_test/src/com/itheima/git/Mygit.java
+++ mygit/git_test/src/com/itheima/git/Mygit.java
@@ -2,8 +2,12 @@ package com.itheima.git;
 
 public class Mygit {
     public static void main(String[] args) {
-        if(1+1 == 2){
+        int i =1;
+        while(i==5){
             System.out.println("何师傅是色图搜查官");
+            i++;
+        }
+        if(1+1 == 2){
             System.out.println("何师傅是色图搜查官");
             System.out.println("何师傅是色图搜查官");
             System.out.println("何师傅是色图搜查官");
